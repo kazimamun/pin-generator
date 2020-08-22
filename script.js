eb8116b82f7527b4db4b933c8e042338fe97734d
kazimamun <41811956+kazimamun@users.noreply.github.com>
2020-08-22 23:47:21 +0600

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//hide notification 
-document.querySelectorAll(".notify").style.display = 'none';
+hideFromDisplay("#not-matched", "#matched", "#try-again")
 
 //generate random number
 const pinGenerate = document.querySelector('#pin-btn');
@@ -8,8 +7,9 @@ pinGenerate.addEventListener('click',()=>{
     document.querySelector('#pin-monitor').value = random;
     //second time generate new number after getting result and also below work done
     document.getElementById('try').innerText = 3;
-    document.querySelectorAll(".notify").style.display = 'none';
-    document.querySelector('#monitor').value = '';
+    hideFromDisplay("#not-matched", "#matched", "#try-again")
+    document.querySelector('#monitor').value = '';    
+    document.getElementById('submit').disabled  = false;
 })
 
 //matching and show notification
@@ -21,17 +21,25 @@ submitBtn.addEventListener('click',function(){
     let countdown = parseInt(document.getElementById('try').innerText);
     if(pinMonitor == inputMonitor){
         document.querySelector("#matched").style.display = 'block';
-        document.querySelector('#not-matched').style.display = 'none';
+        hideFromDisplay('#not-matched');
     }else{
         document.querySelector('#not-matched').style.display = 'block';
-        document.querySelector("#matched").style.display = 'none';
+        hideFromDisplay("#matched");
 
-        if(countdown < 1){
+        if(countdown <= 0){
             document.getElementById('submit').disabled  = true;
-            document.querySelector('#not-matched').style.display = 'none';
             document.querySelector("#try-again").style.display = 'block';
+            hideFromDisplay('#not-matched');
         }else{
             document.getElementById('try').innerText = countdown - 1;
         }     
     }
 })
+
+//hide notification function
+function hideFromDisplay(){
+    const args = [...arguments];
+    args.map(arg => {
+        document.querySelector(arg).style.display = 'none';
+    })
+}
